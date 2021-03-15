58f913a121f63bbb489f9dba3100aa174e80f91d
DenisKondratov <denys.kondratov@gmail.com>
2021-03-15 20:52:53 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lastReplaced = str;
+
+  // create regex string sth like ['()', '[]', '||'] but with escaping
+  const regexStringArray = bracketsConfig.map(([openSign, closeSign]) => `${getEscapedSign(openSign)}${getEscapedSign(closeSign)}`);
+  // create regex
+  const regex = new RegExp(regexStringArray.join('|'));
+
+  // while regex passes
+  while (lastReplaced.search(regex) >= 0) {
+      // replace ['()', '[]', '||'] with nothing
+      lastReplaced = lastReplaced.replace(regex, '');
+  }
+
+  // if everything was replaced = check returns true
+  return !lastReplaced;
+}
+
+function getEscapedSign(sign) {
+  return ['(', ')', '|', '[', ']'].includes(sign) ? `\\${sign}` : sign;
 }
